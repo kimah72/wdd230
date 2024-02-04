97ac4d990c73f351cb29d25728b8e5e05a7e6b6f
kimah72 <min23005@byui.edu>
2024-02-03 20:15:45 -0700

    Not happy with it. Turning in as is.

---
+++ chamber/scripts/getdates.js
@@ -0,0 +1,16 @@
+// New script for current year 
+const currentyear = {year: 'numeric'};
+document.getElementById('currentyear').textContent = new Date().toLocaleDateString('en-US', currentyear);
+
+// New script for date last modified
+document.getElementById('lastModified').textContent = "Last Modification: " + document.lastModified;
+
+// Hamburger menu
+
+// const hamButton = document.querySelector('#menu');
+// const mainnav = document.querySelector('.bullet')
+
+// hamButton.addEventListener('click', () => {
+// 	mainnav.classList.toggle('show');
+// 	hamButton.classList.toggle('show');
+// });
