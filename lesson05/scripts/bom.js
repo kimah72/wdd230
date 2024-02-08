4f823291efd4cbe2e0e4c4b5efb5a1912e7721b1
kimah72 <min23005@byui.edu>
2024-02-07 21:36:09 -0700

    new group push - assignment

---
+++ lesson05/scripts/bom.js
@@ -0,0 +1,25 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () => {
+	if (input.value !="") 
+    {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton.textContent = "❌";
+        li.append(deleteButton);
+        list.append(li);
+
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+            input.focus();
+            input.value = "";
+        });
+    }
+    else 
+    {
+        alert("You must enter a value.");
+    }
+});
