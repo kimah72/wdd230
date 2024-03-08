54dc231a3fc62ed314e15749be41c6191e2bc698
kimah72 <min23005@byui.edu>
2024-03-07 20:26:01 -0700

    final push for thursday

---
+++ scripts/links.js
@@ -0,0 +1,30 @@
+const baseURL = "https://kimah72.github.io/wdd230/";
+const linksURL = "https://kimah72.github.io/wdd230/data/links.json";
+const ul = document.querySelector(".links");
+
+async function getLinks(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    displayLinks(data.lessons);
+};
+
+  const displayLinks = (weeks) => {
+    weeks.forEach((week) => {
+        const h4 = document.createElement("h4");
+        h4.textContent = `Lesson ${week.lesson}: `;
+
+        week.links.forEach((lesson) => {
+            const a = document.createElement("a");
+
+            a.setAttribute("href", `${lesson.url}`);
+            a.textContent = `${lesson.title}  -  `;
+
+            h4.appendChild(a);
+
+        });
+        ul.appendChild(h4);
+    });
+};
+
+getLinks(linksURL);
