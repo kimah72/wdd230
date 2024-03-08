b95748944fe0adab882446a1cff72e1163eb2b7c
kimah72 <min23005@byui.edu>
2024-03-07 19:15:39 -0700

    prophet assignmet

---
+++ lesson09/scripts/prophets.js
@@ -0,0 +1,35 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.table(data.prophets);
+    displayProphets(data.prophets);
+  }
+  const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        const card = document.createElement("section");
+        const fullName = document.createElement("h2");
+        const birthDate = document.createElement("p");
+        const birthPlace = document.createElement("p");
+        const portrait = document.createElement("img");
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+        portrait.setAttribute("src", `${prophet.imageurl}`);
+        portrait.setAttribute("alt", `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute("loading", "lazy");
+        portrait.setAttribute("width", "340");
+        portrait.setAttribute("height", "440");
+
+        card.appendChild(fullName);
+        card.appendChild(birthDate);
+        card.appendChild(birthPlace);
+        card.appendChild(portrait);
+        cards.appendChild(card);
+    })
+}
+
+  getProphetData();
