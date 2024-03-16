b04521cbe7b9c9c5e2d5bf0cce1d0c3ed15c54fd
kimah72 <min23005@byui.edu>
2024-03-16 11:57:32 -0600

    collabroation activity - weather api

---
+++ scripts/weather.js
@@ -0,0 +1,34 @@
+// variables for temp, icon and description
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption')
+// API variables and url
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=40.115&lon=-111.6549&appid=f333fbf945ff940b11bdcc892d3ac53e&units=imperial';
+
+// Fetch weather data from openweathermao.org
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    }   catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${Math.round(data.main.temp)}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    let desc = data.weather[0].description;
+    desc = desc.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' '); 
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+}
